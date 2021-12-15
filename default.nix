2a28496920441c63d49dbcc5a1fbd8929e00f991
nakata5321 <makar5321@gmail.com>
2021-12-15 13:22:32 +0300

    main changes

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 
 mkRosPackage rec {
   name = "${pname}-${version}";
-  pname = "gakachu_frontend";
+  pname = "gakachu_backend";
   version = "master";
 
   src = ./.;
@@ -14,10 +14,10 @@ mkRosPackage rec {
   propagatedBuildInputs = [ robonomics_comm-nightly python3Packages.fastapi python3Packages.uvicorn ];
 
   meta = with lib; {
-    description = "GakaChu Frontend";
+    description = "Gakachu backend";
     homepage = http://github.com/vourhey/gakachu_frontend;
     license = licenses.bsd3;
-    maintainers = with maintainers; [ vourhey ];
+    maintainers = with maintainers; [ vourhey, nakata5321 ];
   };
 }
 
